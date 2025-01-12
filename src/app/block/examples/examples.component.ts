f0abe17f1cc563fb5e2f21f8ec04ebcd09e73762
evelindev <evelinvigberg@gmail.com>
2025-01-13 01:55:17 +0400

    add: examples component

---
+++ src/app/block/examples/examples.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'examples-block',
+  imports: [],
+  templateUrl: './examples.component.html',
+  styleUrl: './examples.component.css'
+})
+export class ExamplesComponent {
+
+}

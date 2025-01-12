2e55ae1cb3a2417f0bf5688bc38916119f7ece00
evelindev <evelinvigberg@gmail.com>
2025-01-13 01:57:44 +0400

    add: about component

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'about-page',
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}

8e4f8f942999f5362ad2b6288ccd881021314077
evelindev <evelinvigberg@gmail.com>
2025-01-13 01:57:09 +0400

    add: team component

---
+++ src/app/block/team/team.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'team-block',
+  imports: [],
+  templateUrl: './team.component.html',
+  styleUrl: './team.component.css'
+})
+export class TeamComponent {
+
+}

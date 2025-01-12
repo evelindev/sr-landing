ee2b2882571000f2d9eacbd5d12a5429c893464f
evelindev <evelinvigberg@gmail.com>
2025-01-13 01:56:37 +0400

    add: services component

---
+++ src/app/block/our-services/services.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'services-block',
+  imports: [],
+  templateUrl: './services.component.html',
+  styleUrl: './services.component.css'
+})
+export class ServicesComponent {
+
+}

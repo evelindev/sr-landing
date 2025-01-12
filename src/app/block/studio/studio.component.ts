2bfc3abd5d33f1b3625edc681d0750fbea434efe
evelindev <evelinvigberg@gmail.com>
2025-01-13 01:56:59 +0400

    add: studio component

---
+++ src/app/block/studio/studio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'studio-block',
+  imports: [],
+  templateUrl: './studio.component.html',
+  styleUrl: './studio.component.css'
+})
+export class StudioComponent {
+
+}

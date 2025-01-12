195e18093a395229db8aad97d4947f30eaf5e4ff
evelindev <evelinvigberg@gmail.com>
2025-01-13 01:54:38 +0400

    add: advantages component

---
+++ src/app/block/advantages/advantages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'advantages-block',
+  imports: [],
+  templateUrl: './advantages.component.html',
+  styleUrl: './advantages.component.css'
+})
+export class AdvantagesComponent {
+
+}

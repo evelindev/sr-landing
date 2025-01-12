e3ed3566655b3b2ebaafd73442ff7a3472fd22ca
evelindev <evelinvigberg@gmail.com>
2025-01-13 01:56:47 +0400

    add: sr component

---
+++ src/app/block/sr/sr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sr-block',
+  imports: [],
+  templateUrl: './sr.component.html',
+  styleUrl: './sr.component.css'
+})
+export class SrComponent {
+
+}
